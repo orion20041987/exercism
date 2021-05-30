66f0aeac97abd00d66f7c329932dcb3c51fac58c
orion20041987 <orion20041987@gmail.com>
2021-05-30 20:38:34 +0300

    sync PC with MAC

--- difference-of-squares/difference_of_squares.py
+++ difference-of-squares/difference_of_squares.py
@@ -1,17 +1,27 @@
 def square_of_sum(number):
-    for i in range(number):
+    sum = 0
+    for i in range(1, number + 1):
         sum += i
     square_of_sum_result = sum ** 2
     return square_of_sum_result
 
 
 def sum_of_squares(number):
-    for i in range(number):
-        res = 0
+    res = 0
+    for i in range(1, number + 1):
         res += (i ** 2)
     sum_of_squares_result = res
     return sum_of_squares_result
 
 
 def difference_of_squares(number):
+    sum = 0
+    for i in range(1, number + 1):
+        sum += i
+    square_of_sum_result = sum ** 2
+    res = 0
+    for i in range(1, number + 1):
+        res += (i ** 2)
+    sum_of_squares_result = res
     difference_of_squares_result = square_of_sum_result - sum_of_squares_result
+    return difference_of_squares_result
