91793daac3b5ff1141fd3a12968e2737b200970d
orion20041987 <orion20041987@gmail.com>
2021-05-30 18:16:09 +0300

    synchro mac and pc prj

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -4,14 +4,12 @@ import re
 def is_isogram(string):
     if string == '':
         return True
-    letter_list = re.findall(r'\w+', string.lower())
-    res = []
-    for item in letter_list:
-        res += item
-
-    tmp = [[item, res.count(item)] for item in set(res)]
-    for a in tmp:
+    parsed_string = re.findall(r'\w+', string.lower())
+    letter_list = []
+    for item in parsed_string:
+        letter_list += item
+    counter = [[item, letter_list.count(item)] for item in set(letter_list)]
+    for a in counter:
         if a[1] != 1:
             return False
     return True
-
