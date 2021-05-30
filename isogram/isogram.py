3251080cd3f30c91dc15d4e3c808433f9f58e5b8
orion20041987 <orion20041987@gmail.com>
2021-05-30 15:57:14 +0300

    synchro mac and pc prj

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -6,9 +6,19 @@ def is_isogram(string):
     res = []
     for item in letter_list:
         res += item
-    print(res)
-    for item in res:
-        print(res.count(item))
+    # print(res)
+    # for item in res:
+    #     print(res.count(item))
+
+    print([[item, res.count(item)] for item in set(res)])
+    tmp = [[item, res.count(item)] for item in set(res)]
+    for a in tmp:
+        if a[1] != 1:
+            return False
+        return True
 
 
-print(is_isogram('abc'))
+
+
+print(is_isogram('adfkgrkjrengkejfopwnfbc'))
+print(is_isogram('adcdrfvtgbyhnmju'))
