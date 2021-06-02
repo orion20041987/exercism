cffc0e11abc9c68ff82d2171805361f74bd49367
orion20041987 <orion20041987@gmail.com>
2021-06-02 22:29:49 +0300

    sync Mac with PC (phone_number_second_iteration)

--- phone-number/phone_number.py
+++ phone-number/phone_number.py
@@ -3,40 +3,48 @@ import re
 
 class PhoneNumber:
     def __init__(self, num):
-        self.num = re.findall(r'\d+', num)
-        res = ''
-        for item in self.num:
-            res += item
-        if len(res) == 10 and re.findall(r'[2-9]{1}[0-9]{2}[2-9]{1}[0-9]{6}', res):
-            res = '1' + res
-            print(f'add 1 at number. result is: {res}')
-        else:
-            print(f'Единицу не добавляем {res}')
-        if not re.findall(r'[1]{1}[2-9]{1}[0-9]{2}[2-9]{1}[0-9]{6}', res):
-            print('Value error')
-        else:
-            self.res = res
+        self.num = num
+
 
 
 
     def number(self):
-        res = self.res
-        print(f'qqq {res[0:10]}')
-        return res[0:10]
+        parsed_num = [digit for digit in self.num if digit.isdigit()]
+        if len(parsed_num) < 10 or len(parsed_num) > 11:
+            # print('_______ValueError(Cell number must be 10 or 11 digits long)\n')
+            raise ValueError('Cell number must be 10 or 11 digits long')
+        elif len(parsed_num) == 11 and parsed_num[0] != "1":
+            # print('_______ValueError(Country code must be equal "1")\n')
+            raise ValueError('Country code must be equal "1"')
+        elif len(parsed_num) == 10:
+            # print(f'Original number: {parsed_num}')
+            parsed_num.insert(0, '1')
+            # print(f'Added country code {parsed_num}')
+        else:
+            pass
+            # print(f'Cell number is valid: {parsed_num}')
+
+        if int(parsed_num[1]) < 2 or int(parsed_num[4]) < 2:
+            # print('_______ValueError(Wrong cell number format (NXX)-NXX-XXXX))\n')
+            raise ValueError('Wrong cell number format (NXX)-NXX-XXXX))')
+        elif len(parsed_num) == 11:
+            cell_number = ''.join(parsed_num[1:])
+            # print(f'Result number is: {cell_number}\n')
+            return cell_number
 
     def pretty(self):
         pass
 
 
 if __name__ == "__main__":
-    # number = PhoneNumber("1(223) 456-7890").number()
-    # number = PhoneNumber("123456789").number()
-    # number = PhoneNumber("(223) 456-7890").number()
-    # number = PhoneNumber("1223456789").number()
-    # number = PhoneNumber("(223) 456-7890").number()
-    # number = PhoneNumber("(223) 456-7890").number()
-    # number = PhoneNumber("(223) 456-7890").number()
-    # number = PhoneNumber("(223) 456-7890").pretty()
-    # number = PhoneNumber('+1 (111) 111-1111').pretty()
-    # number = PhoneNumber('21234567890').pretty()
-    # PhoneNumber("22234567890")
+    number = PhoneNumber("1(223) 456-7890").number()
+    number = PhoneNumber("123456789").number()
+    number = PhoneNumber("(223) 456-7890").number()
+    number = PhoneNumber("1223456789").number()
+    number = PhoneNumber("(223) 456-7890").number()
+    number = PhoneNumber("(223) 456-7890").number()
+    number = PhoneNumber("(223) 456-7890").number()
+    number = PhoneNumber("(223) 456-7890").number()
+    number = PhoneNumber('+1 (111) 111-1111').number()
+    number = PhoneNumber('21234567890').number()
+    number = PhoneNumber('22234567890').number()
