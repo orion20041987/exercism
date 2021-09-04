22d36e52d3105ded8d359a2d46d7e250c01a3872
orion20041987 <orion20041987@gmail.com>
2021-09-04 15:25:37 +0300

    twelve_days_ps_solved

--- twelve-days/twelve_days.py
+++ twelve-days/twelve_days.py
@@ -6,17 +6,16 @@ def recite(start_verse, end_verse):
     for n in range(start_verse, end_verse + 1):
         days_list = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth', 'ninth', 'tenth',
                      'eleventh', 'twelfth']
-        lyrics_list = ['a Partridge in a Pear Tree', 'two Turtle Doves', 'three French Hens', 'four Calling Birds',
-                       'five Gold Rings', 'six Geese-a-Laying', 'seven Swans-a-Swimming', 'eight Maids-a-Milking',
-                       'nine Ladies Dancing', 'ten Lords-a-Leaping', 'eleven Pipers Piping', 'twelve Drummers Drumming']
+        gifts_list = ['a Partridge in a Pear Tree', 'two Turtle Doves', 'three French Hens', 'four Calling Birds',
+                      'five Gold Rings', 'six Geese-a-Laying', 'seven Swans-a-Swimming', 'eight Maids-a-Milking',
+                      'nine Ladies Dancing', 'ten Lords-a-Leaping', 'eleven Pipers Piping', 'twelve Drummers Drumming']
         separator = ', '
-        lyrics_list_slice = lyrics_list[n - 1: 0: -1]
+        gifts_list_slice = gifts_list[n - 1: 0: -1]
         if n == 1:
-            variable_text = lyrics_list[n - 1]
+            variable_text = gifts_list[n - 1]
         else:
-            variable_text = separator.join(lyrics_list_slice) + f', and {lyrics_list[0]}'
+            variable_text = separator.join(gifts_list_slice) + f', and {gifts_list[0]}'
 
         output_text = f'On the {days_list[n - 1]} day of Christmas my true love gave to me: {variable_text}.'
         output_data.append(output_text)
     return output_data
-
