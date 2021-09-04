7d573831a89d6947eb997676dc0cb47ea5155fea
orion20041987 <orion20041987@gmail.com>
2021-09-04 15:02:59 +0300

    twelve_days_ps_first_verion

--- twelve-days/twelve_days.py
+++ twelve-days/twelve_days.py
@@ -1,41 +1,22 @@
 def recite(start_verse, end_verse):
-    lyrics_dict = {
-        '1': 'On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.',
-        '2': 'On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.',
-        '3': 'On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, '
-             'and a Partridge in a Pear Tree.',
-        '4': 'On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, '
-             'two Turtle Doves, and a Partridge in a Pear Tree.',
-        '5': 'On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, '
-             'three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.',
-        '6': 'On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, '
-             'four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.',
-        '7': 'On the seventh day of Christmas my true love gave to me: seven Swans-a-Swimming, six Geese-a-Laying, '
-             'five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear '
-             'Tree.',
-        '8': 'On the eighth day of Christmas my true love gave to me: eight Maids-a-Milking, seven Swans-a-Swimming, '
-             'six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, '
-             'and a Partridge in a Pear Tree.',
-        '9': 'On the ninth day of Christmas my true love gave to me: nine Ladies Dancing, eight Maids-a-Milking, '
-             'seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, '
-             'two Turtle Doves, and a Partridge in a Pear Tree.',
-        '10': 'On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, nine Ladies Dancing, '
-              'eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling '
-              'Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.',
-        '11': 'On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, '
-              'nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, '
-              'five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear '
-              'Tree.',
-        '12': 'On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers '
-              'Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, '
-              'six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, '
-              'and a Partridge in a Pear Tree.'
-    }
+    if start_verse < 0 or end_verse > 12 or start_verse > end_verse:
+        raise ValueError('Incorrect input indexes')
 
-    lyric_output_list = []
+    output_data = []
     for n in range(start_verse, end_verse + 1):
-        lyric_output_list.append(lyrics_dict[str(n)])
-    return lyric_output_list
+        days_list = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth', 'ninth', 'tenth',
+                     'eleventh', 'twelfth']
+        lyrics_list = ['a Partridge in a Pear Tree', 'two Turtle Doves', 'three French Hens', 'four Calling Birds',
+                       'five Gold Rings', 'six Geese-a-Laying', 'seven Swans-a-Swimming', 'eight Maids-a-Milking',
+                       'nine Ladies Dancing', 'ten Lords-a-Leaping', 'eleven Pipers Piping', 'twelve Drummers Drumming']
+        separator = ', '
+        lyrics_list_slice = lyrics_list[n - 1: 0: -1]
+        if n == 1:
+            variable_text = lyrics_list[n - 1]
+        else:
+            variable_text = separator.join(lyrics_list_slice) + f', and {lyrics_list[0]}'
 
+        output_text = f'On the {days_list[n - 1]} day of Christmas my true love gave to me: {variable_text}.'
+        output_data.append(output_text)
+    return output_data
 
-# recite(1, 2)
